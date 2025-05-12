bc29da05c85bd7d3ee6f6efa60bf5eb714e4f8e3
yak155775 <aryatirtana155@gmail.com>
2025-05-12 21:44:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let audio = document.getElementById("c:\Users\hp\Downloads\Sempurna, Nanti Kita Seperti Ini, Penjaga Hati (Lirik) - Andra & The Backbone.mp3");
+    audio.volume = 0.5; // Mengatur volume
+});
